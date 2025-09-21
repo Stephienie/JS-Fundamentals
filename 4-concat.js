89663cc555407305edc56b72310d0207ea91df8c
Stephienie <thatmaverickme@gmail.com>
2025-09-21 16:30:40 +0100

    Done

--- 4-concat.js
+++ 4-concat.js
@@ -1,6 +1,5 @@
-const firstArg = process.argv[2]
-if (firstArg === undefined) {
-console.log("No argument");
-} else {
-console.log(firstArg)
-}
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+console.log(`${arg1} is ${arg2}`);
+
+
